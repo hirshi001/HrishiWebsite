2d85c3e3b5b800e267698457738dfa0dd0ec2ba9
Hrishikesh Ingle <ketchhrishikesh.ingleup@gmail.com>
2023-06-17 13:54:22 -0700

    test

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
+var scrollClick = false;
+
+addEventListener("scroll", function() {
+    if (scrollClick) {
+        scrollClick = false;
+        return;
+    }
+    let currentScrollPos = window.pageYOffset;
+    let navbar = document.getElementById("navbar");
     if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
+        navbar.style.top = "0";
     } else {
-        let navbar = document.getElementById("navbar");
-        console.log(navbar.offsetHeight);
         navbar.style.top = "-"+navbar.offsetHeight + "px"
     }
     prevScrollpos = currentScrollPos;
+})
+
+addEventListener("mousemove", function(e) {
+    let currentScrollPos = window.pageYOffset;
+    let y = e.clientY;
+
+    let navbar = document.getElementById("navbar");
+    if (y < navbar.offsetHeight) {
+        document.getElementById("navbar").style.top = "0";
+    }
+    prevScrollpos = currentScrollPos;
+
+})
+
+function onNavBarButtonClick(){
+    scrollClick = true;
+
 }
